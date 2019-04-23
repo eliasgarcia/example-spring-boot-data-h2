0f31fe0e73d0b604aaaa80006bb2579f113ce3b7
Elias Garcia <garcia.elias.emg@gmail.com>
2019-04-23 13:57:50 -0300

    push a master

--- src/main/java/com/example/abmBasico/repository/BookRepository.java
+++ src/main/java/com/example/abmBasico/repository/BookRepository.java
@@ -9,4 +9,5 @@ import com.example.abmBasico.domain.Book;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long>{
 	Book findByName(String name);// nuevoBranch modif 2, mergeado
+	//sigo en master
 }
